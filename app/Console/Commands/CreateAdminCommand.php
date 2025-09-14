b51cceecea11f6e2a698611ca697eb981c0cc137
Rintu Kumar Chowdhury <rintuchy.cse@gmail.com>
2025-09-14 20:34:36 +0600

    Implement Custom Artisan Command to add admin user/employee

--- app/Console/Commands/CreateAdminCommand.php
+++ app/Console/Commands/CreateAdminCommand.php
@@ -2,29 +2,35 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Employee;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
 
 class CreateAdminCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:create-admin-command';
+    protected $signature = 'make:admin 
+        {first_name : First name}
+        {last_name : Last name}
+        {email : Email address}
+        {password : Password}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'Create an admin employee';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
+    public function handle(): int
     {
-        //
+        $employee = Employee::create([
+            'first_name' => $this->argument('first_name'),
+            'last_name' => $this->argument('last_name'),
+            'email' => $this->argument('email'),
+            'password' => Hash::make($this->argument('password')),
+            'position' => 'System Administrator',
+            'department_id' => 1, // IT department
+            'hire_date' => now(),
+        ]);
+
+        $this->info('Admin employee created successfully!');
+        $this->line('Email: ' . $employee->email);
+
+        return 0;
     }
 }
